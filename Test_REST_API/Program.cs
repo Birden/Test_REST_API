eef415488b261e876c2e728e6b1accf0b2c0367c
Denis Biryukov <dbir@ngs.ru>
2019-05-05 23:20:01 +0700

    1

--- Test_REST_API/Program.cs
+++ Test_REST_API/Program.cs
@@ -3,13 +3,41 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Nancy;
 
 namespace Test_REST_API
 {
+    class Person
+    {
+        string name;
+        int age;
+        //DateTime bornDate;
+
+        public int? CreatePerson(string name, DateTime bornDate)
+        {
+            int age = Convert.ToInt32((DateTime.Now - bornDate).TotalDays / 365);
+            if ((name == "") || (age < 0) || (age > 120))
+                return null;
+            else
+            {
+                this.name = name;
+                this.age = age;
+                Console.WriteLine("Create person <"+name+"> with age " + age.ToString());
+            }
+            return 0;
+        }
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
+            Person ps1 = new Person();
+            List<Person> pList = new List<Person>();
+
+
+            ps1.CreatePerson("Alex", Convert.ToDateTime("01.08.1979"));
+            Console.ReadKey();
         }
     }
 }
